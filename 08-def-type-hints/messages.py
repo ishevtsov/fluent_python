b20e014b40ed60d0a9bc3fa86dbd66208853a26c
Igor Shevtsov <infoshevtsov@gmail.com>
2024-08-27 17:38:57 +0100

    finished with def-type-hints

---
+++ 08-def-type-hints/messages.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+def show_count(count: int, singular: str, plural: Optional[str] = None) -> str:
+	if count == 1:
+		return f'1 {singular}'
+	count_str = str(count) if count else 'no'
+	if not plural:
+		plural = singular + 's'
+	return f'{count_str} {plural}'
+
+def main():
+	print(show_count(99, 'bird'))
+	print(show_count(1, 'bird'))
+	print(show_count(0, 'bird'))
+
+if __name__ == '__main__':
+	main()
